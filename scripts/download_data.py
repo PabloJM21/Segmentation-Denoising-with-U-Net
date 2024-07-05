433b9dd88286a95e4f904f0cc5e4d70d6256f3f0
PabloJM21 <pablo.jahnen@stud.uni-goettingen.de>
2024-07-05 15:05:53 +0200

    Update download_data.py

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -1,1 +1,14 @@
-
+# download the data
+data_folder = os.path.join(root_folder, "kaggle-data")
+url = "https://github.com/stardist/stardist/releases/download/0.1.0/dsb2018.zip"
+if not os.path.exists(data_folder):
+    os.makedirs(data_folder)
+    tmp_path = os.path.join(data_folder, "data.zip")
+    with requests.get(url) as r:
+        with open(tmp_path, "wb") as f:
+            f.write(r.content)
+    with zipfile.ZipFile(tmp_path, "r") as f:
+        f.extractall(data_folder)
+    os.remove(tmp_path)
+data_folder = "data/dsb2018"
+assert os.path.exists(data_folder)
