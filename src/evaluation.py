b72d73975dc79f58749cb9346cb8723ef18fcb7f
PabloJM21 <pablo.jahnen@stud.uni-goettingen.de>
2024-07-08 00:47:04 +0200

    Update evaluation.py

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,1 +1,102 @@
+# We load and normalize the test images
+test_images = [imageio.imread(img) for img in test_image_paths]
+test_images = [(im.astype("float32") - mean) / std for im in test_images]
+test_masks = [imageio.imread(mask_path) for mask_path in test_mask_paths]
+
+# We check out instance segmentation for a few test images
+counter = 0
+with torch.no_grad():
+    for im, mask in zip(test_images, test_masks):
+        if counter > 3:
+            break
+        # predict with the model and apply sigmoid to map the prediction to the range [0, 1]
+        pred = model(torch.from_numpy(im[None, None]).to(device))
+        pred = torch.sigmoid(pred).cpu().numpy().squeeze()
+        # get tbe nucleus instance segmentation by applying connected components to the binarized prediction
+        nuclei = label(pred > 0.5)
+        fig, ax = plt.subplots(1, 4, figsize=(16, 16))
+        ax[0].axis("off")
+        ax[0].imshow(im, cmap="gray")
+        ax[1].axis("off")
+        ax[1].imshow(mask, cmap=get_random_colors(mask), interpolation="nearest")
+        ax[2].axis("off")
+        ax[2].imshow(pred, cmap="gray")
+        ax[3].axis("off")
+        ax[3].imshow(nuclei, cmap=get_random_colors(nuclei), interpolation="nearest")
+        plt.show()
+        counter += 1
+
+# compute f1 score based on true positives, false positives and false negatives
+def f1(tp, fp, fn):
+    return (2*tp)/(2*tp+fp+fn) if tp > 0 else 0
+
+
+# compute the matches between predicted object (seg) and ground-truth objects (mask)
+def compute_scores(seg, mask):
+    # the contingency table function computes the overlap (intersection) between all objects in the two segmentations 
+    overlap = contingency_table(seg, mask).toarray()
+    # compute the total number of pixels for each object
+    n_pixels_pred = np.sum(overlap, axis=0, keepdims=True)
+    n_pixels_true = np.sum(overlap, axis=1, keepdims=True)
+    # normalize the overlap by the union to compute the IOU
+    eps = 1e-7
+    ious = overlap / np.maximum(n_pixels_pred + n_pixels_true - overlap, eps)
+    # ignore matches with zero (= background)
+    ious = ious[1:, 1:]
+    # compute the number of true objects (in mask), predicted object (in seg)
+    # and the maximal number of matches (minimum of the too)
+    n_pred, n_true = ious.shape
+    n_matched = min(n_pred, n_true)
+    return n_true, n_matched, n_pred, ious
+
+
+# compute the true positives (= matches with a iou bigger than the threshold)
+def compute_tps(ious, n_matched, threshold):
+    not_trivial = n_matched > 0 and np.any(ious >= threshold)
+    if not_trivial:
+        # compute optimal matching with iou scores as tie-breaker
+        costs = -(ious >= threshold).astype(float) - ious / (2*n_matched)
+        pred_ind, true_ind = linear_sum_assignment(costs)
+        assert n_matched == len(true_ind) == len(pred_ind)
+        match_ok = ious[pred_ind, true_ind] >= threshold
+        tp = np.count_nonzero(match_ok)
+    else:
+        tp = 0
+    return tp
+
+
+# compute the f1 score between predicted (seg) and ground-truth (mask) objects for the given threshold
+def f1_score(seg, mask, threshold=0.5):
+    if seg.sum() == 0:
+        return 0.0
+    n_true, n_matched, n_pred, scores = compute_scores(seg, mask)
+    tp = compute_tps(scores, n_matched, threshold)
+    fp = n_pred - tp
+    fn = n_true - tp
+    return f1(tp, fp, fn)
+
+
+# Compute the average f1 score of all the test images
+f1s = []
+model.eval()
+with torch.no_grad():
+    for im, mask in tqdm.tqdm(zip(test_images, test_masks), total=len(test_images)):
+        # the model can only predict images with a spatial size that is divisible by 16
+        # if it isn't we just cat a few pixels to make it fit
+        if any(sh % 16 != 0 for sh in im.shape):
+            crop = tuple(
+                slice(0, -(sh%16)) for sh in im.shape
+            )
+            im = im[crop]
+            mask = mask[crop]
+        input_ = torch.from_numpy(im[None, None]).to(device)
+        pred = model(input_)
+        pred = torch.sigmoid(pred).cpu().numpy().squeeze()
+        labels = label(pred > 0.5)
+
+        f1s.append(f1_score(labels, mask))
+print()
+# print(f1s)
+print("Mean f1 score:", np.mean(f1s))
+
 
