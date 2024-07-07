60fe4283f1e3e144c49013ca71732766bcc2503c
PabloJM21 <pablo.jahnen@stud.uni-goettingen.de>
2024-07-08 00:34:02 +0200

    Update data_exploration.py

--- src/data_exploration.py
+++ src/data_exploration.py
@@ -1,1 +1,44 @@
+image_paths = glob(os.path.join(data_folder, "train", "images", "*.tif"))
+image_paths.sort()
+mask_paths = glob(os.path.join(data_folder, "train", "masks", "*.tif"))
+mask_paths.sort()
+assert len(image_paths) == len(mask_paths)
+
+test_image_paths = glob(os.path.join(data_folder, "test", "images", "*.tif"))
+test_image_paths.sort()
+test_mask_paths = glob(os.path.join(data_folder, "test", "masks", "*.tif"))
+test_mask_paths.sort()
+assert len(test_image_paths) == len(test_mask_paths)
+
+# a function to generate a random color map for a label image
+def get_random_colors(labels):
+    n_labels = len(np.unique(labels)) - 1
+    cmap = [[0, 0, 0]] + np.random.rand(n_labels, 3).tolist()
+    cmap = colors.ListedColormap(cmap)
+    return cmap
+
+def plot_sample(image_path, mask_path):
+    image, mask = imageio.imread(image_path), imageio.imread(mask_path)
+    fig, ax = plt.subplots(1, 2)
+    ax[0].axis("off")
+    ax[0].imshow(image, cmap="gray")
+    # visualize the masks with random colors
+    ax[1].axis("off")
+    ax[1].imshow(mask, cmap=get_random_colors(mask), interpolation="nearest")
+    plt.show()
+
+# plot the first 4 images
+for i in range(4):
+    plot_sample(image_paths[i], mask_paths[i])
+
+
+# we find how many images we have in the training set
+# and find all (unique) shapes of the images in the training set
+print(f"Number of images in the training set: {len(image_paths)}")
+unique_dims = set()
+for img_path in image_paths:
+    img = imageio.imread(img_path)
+    unique_dims.add(img.shape)
+unique_dims
+
 
