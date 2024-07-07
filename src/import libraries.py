76a1d516f134e3f9cdcd8920524c4f2410bcfa73
PabloJM21 <pablo.jahnen@stud.uni-goettingen.de>
2024-07-08 00:18:59 +0200

    Update import libraries.py

--- src/import libraries.py
+++ src/import libraries.py
@@ -1,1 +1,46 @@
+import os
+import tqdm
+import zipfile
+import requests
+from glob import glob
+from pathlib import Path
 
+import numpy as np
+import seaborn as sns
+import imageio.v2 as imageio
+from matplotlib import colors
+import matplotlib.pyplot as plt
+
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+
+from skimage.measure import label
+from skimage.util import random_noise
+from scipy.optimize import linear_sum_assignment
+from skimage.metrics import contingency_table, peak_signal_noise_ratio
+
+#Temporary fix for importing skimage.segmentation on Kaggle
+
+# let's check if the import works or not
+from skimage.segmentation import find_boundaries, watershed
+
+# DO NOT CHANGE
+def fix_scipy_imports(path="/opt/conda/lib/python3.10/site-packages/skimage/morphology/convex_hull.py"):
+    with open(path, "r") as f:
+        file = f.read()
+
+    i = file.find("\n", 49)
+    new_file = file[:i+1] + "# " + file[i+1:]
+
+    with open(path, "w") as f:
+        f.write(new_file)
+
+# the issue should now be resolved. let's try it out.
+try:
+    from skimage.segmentation import find_boundaries, watershed
+except ImportError:
+    fix_scipy_imports()
+    from skimage.segmentation import find_boundaries, watershed
